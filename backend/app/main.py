8e96e030080afb3c6cf33bda99a9762f8a4fc814
jnabulsi <jordanabulsi@gmail.com>
2025-05-20 16:50:58 +1000

    backend settup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,22 +1,17 @@
-import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes import router
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = FastAPI()
 
-# CORS middleware setup
+origins = [
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "*").split(","),
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],  # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
-
-app.include_router(router)
-
 
